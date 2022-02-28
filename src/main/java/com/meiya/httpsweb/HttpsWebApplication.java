7c9d5afd173462d953f5fbb05d7b51d8b4250518
zhanjh <miaque@163. com>
2022-02-28 21:57:51 +0800

    Initial commit

---
+++ src/main/java/com/meiya/httpsweb/HttpsWebApplication.java
@@ -0,0 +1,13 @@
+package com.meiya.httpsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpsWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meiya/httpsweb/HttpsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meiya.httpsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
